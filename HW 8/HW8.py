09b225c5d27938667477c95d26e4f1aa1d00f2f0
EricLiIIT <60123235+EricLiIIT@users.noreply.github.com>
2021-03-09 21:25:37 -0600

    working on the last part

--- HW 8/HW8.py
+++ HW 8/HW8.py
@@ -18,7 +18,14 @@ f) Select all elements from rows 1 and 2 that are in columns 0, 2 and 4.
 import numpy as np
 
 # Question 1
-x = np.array([[1, 2, 3, 4, 5],
-              [6, 7, 8, 9, 10],
-              [11, 12, 13, 14, 15]])
-print(x)
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+new_x = x.reshape(3,5)
+print(new_x) # Reshapes array into 3 x 5 array
+'''
+print("a): ", new_x[1,:]) # a) Prints the second row
+print("b): ", new_x[:3,3]) # b) Prints column 4
+print("c): ", new_x[:1], x[1:2]) # c) Selects rows 0 & 1
+print("d): ", "\n", new_x[:, [1,2,3,4]]) # d) Selects columns 2-4
+print("e): ", new_x[0,4]) # e) Element at row 1 column 4
+'''
+print("f): ", new_x[0:2, 0]) # f Rows 1 & 2 with Columns 0, 2, & 4
