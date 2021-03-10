d3c858f6f91a56e41ba88b659ebb633140785e55
EricLiIIT <60123235+EricLiIIT@users.noreply.github.com>
2021-03-10 15:41:02 -0600

    Finished question 1

--- HW 8/HW8.py
+++ HW 8/HW8.py
@@ -4,6 +4,7 @@ A20419312
 ITMD 413
 Homework 8
 
+Question 1
 (INDEXING AND SLICING ARRAYS) Create an array containing the values 1–15, reshape it
 into a 3-by-5 array, then use indexing and slicing techniques to perform each of the following
 operations:
@@ -14,6 +15,8 @@ d) Select columns 2–4.
 e) Select the element that is in row 1 and column 4.
 f) Select all elements from rows 1 and 2 that are in columns 0, 2 and 4.
 
+Question 2
+
 '''
 import numpy as np
 
@@ -21,11 +24,10 @@ import numpy as np
 x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
 new_x = x.reshape(3,5)
 print(new_x) # Reshapes array into 3 x 5 array
-'''
+
 print("a): ", new_x[1,:]) # a) Prints the second row
 print("b): ", new_x[:3,3]) # b) Prints column 4
 print("c): ", new_x[:1], x[1:2]) # c) Selects rows 0 & 1
 print("d): ", "\n", new_x[:, [1,2,3,4]]) # d) Selects columns 2-4
 print("e): ", new_x[0,4]) # e) Element at row 1 column 4
-'''
-print("f): ", new_x[0:2, 0]) # f Rows 1 & 2 with Columns 0, 2, & 4
+print("f): ", new_x[[1,2],:][:, [0,2,4]]) # f Rows 1 & 2 with Columns 0, 2, & 4
